2ea30a4cf3335964ab064f37d666126cc1d51ef8
Santosh Shah <santoshofficial679@gmail.com>
2023-11-30 09:48:19 +0545

    create-first-comp project is created

--- create-first-comp/src/App.jsx
+++ create-first-comp/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React with Santosh Shah</h1>
+      <h1>Vite + React Components</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
